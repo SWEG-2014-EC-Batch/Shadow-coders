68694884637abc0ff4f5930a3ec48d9b2ffe6d92
Meseret Bolled <122611657+Meseretbolled@users.noreply.github.com>
2023-03-16 21:03:23 +0300

    updated Body Mass Index

--- meseret bolled/Body mass ndex.cpp
+++ meseret bolled/Body mass ndex.cpp
@@ -1,4 +1,4 @@
-//requirment tells if some one is underweghit or overweight that is specific to both gender
+
 
 
 #include <iostream>
@@ -22,13 +22,13 @@ float bmi,height,weight;
     switch(option)
     {
     case 1 :
-        cout<<"Please specifiy your gender. Type M for male, F for female. "; //first check the gender
+        cout<<"Please specifiy your gender. Type M for male, F for female. "; //cross check the gender
     cin>>gender;
 
-    if(gender == 'F'||gender == 'f') //adderess both uppercase or lowercase inputs
+    if(gender == 'F'||gender == 'f') //use both uppercase or lowercase inputs for gender identtfication
         {
-            system("color f");
-            cout<<"Please enter your weight in kilograms";
+            //system("color f");
+            cout<<"Please enter your weight in kilograms";//weight with si unit
             cin>>weight;
             cout<<"Now enter your height in meters ";
             cin>>height;
@@ -38,7 +38,6 @@ float bmi,height,weight;
             if(bmi>=18.0 && bmi<=23.0)//normal range
                 {
                     cout<<"Your BMI is "<<bmi<<" which is normal."<<endl<<"Press any key to start again.";
-
 
                 }
             else if(bmi<18.0)//under weight
@@ -73,6 +72,18 @@ float bmi,height,weight;
                 {
                      cout<<"Your BMI is "<<bmi<<" which is above the recommened weight.";
                 }
+
+
+
+        }
+
+    else //adderess other wrong inputs
+        {
+            //system("color 4");
+            cout<<"\a Invalid input. Please try agian\n";
+
+
+        }
         break;
      case 2 :
         cout<<"Please enter the number of users"; //first check the gender
@@ -84,7 +95,7 @@ float bmi,height,weight;
 
     if(gender == 'F'||gender == 'f') //adderess both uppercase or lowercase inputs
         {
-            system("color f");
+            //system("color f");
             cout<<"Please enter your weight in kilograms ";
             cin>>weight;
             cout<<"Now enter your height in meters ";
@@ -134,13 +145,23 @@ float bmi,height,weight;
 
         }
 
+    else //adderess other wrong inputs
+        {
+            //system("color 4");
+            cout<<"\a Invalid input. Please try agian\n";
+
+
+        }
+        }
 
         break;
     case 3 :
 
     while(q ==1)
    {
-     
+        //system("color f");
+        cout<<"\n Please specify your gender. Type M for male, F for female. "; //first check the gender
+    cin>>gender;
 
     if(gender == 'F'||gender == 'f') //address both uppercase or lowercase inputs
         {
@@ -169,9 +190,9 @@ float bmi,height,weight;
 
     else if(gender == 'M'||gender == 'm') //address both uppercase or lowercase inputs
         {
-            cout<<"Please enter your weight ";
+            cout<<"Please enter your weight in kilograms";
             cin>>weight;
-            cout<<"Now enter your height ";
+            cout<<"Now enter your height in meters";
             cin>>height;
 
             bmi = weight/pow(height,2);
@@ -193,7 +214,12 @@ float bmi,height,weight;
 
         }
 
-    
+    else{
+            //system("color 4");
+            cout<<"\a Invalid input. Please try again\n";
+
+
+        }
         cout<<"If you want to quit the program press 'q' if not press any key "<<endl;
         cin>>quit;
 
@@ -209,5 +235,4 @@ float bmi,height,weight;
         cout<<"invalid input"<<endl;
     }
 return 0;
-}}
-
+}
