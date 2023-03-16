530cce939ad5e8f175389d0a82844a207f9210bf
Meseret Bolled <122611657+Meseretbolled@users.noreply.github.com>
2023-03-16 13:15:02 +0300

    Update symbol checker.cpp

--- meseret bolled/symbol checker.cpp
+++ meseret bolled/symbol checker.cpp
@@ -1,27 +1,47 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
+
 int main()
 {
-  char x;
-  int m;
-  cout<<"enter x: ";
-  cin>>x;
-  if(x>='a'&&x<='z')
-  {
-  cout<<"our value "<<x<<"is a lowercase";}
-  else if(x>='A'&&x<='Z')
-  {
-  cout<<"our char "<<x<<"is an uppercase";
-  }
-  else if(x>='0'&&x<='9'){
+    char x;
+    int m;
+    cout << "Enter a symbol: ";
+    cin>>inPut;
+
+    if(inPut>='a'&&inPut<='z') {
+        cout<<"Your symbol '"<<inPut<<"' is a lowercase"<<endl;
+        if(inPut == 'a'||inPut == 'e'|| inPut == 'i'|| inPut == 'o'|| inPut == 'u'){
+            cout<<"the lowercase '"<<inPut<<"' is a vowel"<<endl;
+        }
+        else{
+            cout<<"the lowercase '"<<inPut<<"' is a consonant"<<endl;
+}
+    }
+    else if(inPut>='A'&&inPut<='Z') {
+        cout<<"Your symbol '"<<inPut<<"' is an uppercase"<<endl;
+        if(inPut == 'A'||inPut == 'E'|| inPut == 'I'|| inPut == 'O'|| inPut == 'U'){
+            cout<<"the uppercase '"<<inPut<<"' is a vowel"<<endl;
+        }
+        else{
+            cout<<"the uppercase '"<<inPut<<"' is a consonant"<<endl;
+        }
+    }
   
-  cout<<"our char "<<x<<"is a digit";}
-  else 
-  {
-cout<<x<<"is a  any character"; }
-  return 0;
-  }
-  
+    else if(inPut>='0'&&inPut<='9'&&inPut%2==0)
+    {
+        cout<<"Your symbol '"<<inPut<<"' is a digit and an even number"<<endl;
+    }
+    else if(inPut>='0'&&inPut<='9'&&inPut%2==1)
+    {
+        cout<<"Your symbol '"<<inPut<<"' is a digit and an odd number"<<endl;}
+    else {
+        cout<<"Your symbol '"<<inPut<<"' is a special character"<<endl;
+    }
+
+    
+    return 0;
+}
    
 
 
