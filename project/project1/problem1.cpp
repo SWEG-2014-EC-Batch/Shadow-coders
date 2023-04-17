c0c998d0c8c7cf8c96c45a2e7a679e36166b298f
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2023-04-17 12:51:31 +0300

    Update problem1.cpp

--- project/project1/problem1.cpp
+++ project/project1/problem1.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+  else
+            {   //cout<<"yee"<<endl;
+                sum1=(s[i]+1)*pow(10,i);
+                sum2=(s[i])*pow(10,i)-1;
+            }
+    }
+    else
+        {
+        sum1+=s[i]*pow(10,i);
+        sum2+=s[i]*pow(10,i);
+        }
+    }
 return 0;
 }
