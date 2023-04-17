583e84610f21170cd26b1b9b1ca89c0ba2b606be
MihretDe <124504706+MihretDe@users.noreply.github.com>
2023-04-17 12:56:41 +0300

    Update problem1.cpp

--- project/project1/problem1.cpp
+++ project/project1/problem1.cpp
@@ -1,6 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
+
 using namespace std;
-int main(){
+
+int main()
+{
+    long int x,y,w=0,m=0;
+    float sum1=0,sum2=0;
+    cout<<"Enter the temp you want to cook with \n";
+    cin>>x;
+    y=x;
+
   else
             {   //cout<<"yee"<<endl;
                 sum1=(s[i]+1)*pow(10,i);
