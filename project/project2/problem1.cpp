dc5561b45c7d9c86243ff97feae58daef71484e3
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2023-04-17 13:44:47 +0300

    Update problem1.cpp

--- project/project2/problem1.cpp
+++ project/project2/problem1.cpp
@@ -277,6 +277,102 @@ if(dataType=='A'||dataType=='a'){
         }
     }
 }
+    else if(dataType=='R'dataType=='r'){
+      for(int i=0;i<13;i++){
+        searchMonthInfo[i]=month[1][i];
+        for(int i=0;i<13;i++){
+            in1[i]=month[2][i];
+            }
+        for(int i=0;i<13;i++){
+            in[i]=((int)(in1[i]/10))*10;
+            }
+        for(int i=0;i<13;i++){
+            deci[i]=(in1[i]-in[i]);
+        }
+    }
+    if(dataRepresentation=='G'||dataRepresentation=='g'){
+        for(int i=0;i<13;i++){
+            cout<<input[i]<<" ";
+            cout<<month[2][i]<<endl;
+        }
+        cout<<endl<<endl;
+        system("cls");
+        cout<<"\t\t\t\t\t\t\trelative rain fall bar graph"<<endl;
+        cout<<"\t\t\t\t\t\t\t--------------------------"<<endl<<endl<<endl;
+        for(int j=12;j>=0;j--){
+            cout<<input[j];
+            if(month[0][j]==5||month[0][j]==10){
+                cout<<"    ";
+            }
+            else if(month[0][j]==3||month[0][j]==11){
+                cout<<"   ";
+            }
+            else if(month[0][j]==6||month[0][j]==7){
+                cout<<" ";
+            }
+            else if(month[0][j]==2||month[0][j]==4||month[0][j]==9||month[0][j]==12||month[0][j]==13){
+                cout<<"  ";
+            }
+            else{
+                cout<<"";
+            }
+
+            if(in[j]-10>=0){
+                cout<<"||";
+                for(int i=0;i<in[j]-10;i+=10){
+                    cout<<"||";
+                    }
+                if(deci[j]>0&&deci[j]<5){
+                    cout<<"";
+                    }
+                else if (deci[j]==5){
+                    cout<<"|||";
+                    }
+                else if(deci[j]>5&&deci[j]<=9){
+                    cout<<"||||";
+                    }
+                }
+                    cout<<endl<<endl;
+                }
+
+        cout<<"        0";
+        for(int i=10;i<=100;i+=10){
+            if(i<10){
+                cout<<"    ";
+            }
+            else{
+                cout<<"    ";
+            }
+            cout<<i;
+            }
+            cout<<endl;
+        }
+
+
+    else if(dataRepresentation=='T'||dataRepresentation=='t'){
+        cout<<"----------------------------------"<<endl;
+        cout<<"|  month    |relative rainfall(%)|"<<endl;
+        cout<<"----------------------------------"<<endl;
+        for(int j=0;j<13;j++){
+            cout<<input[j];
+            if(month[0][j]==5||month[0][j]==10){
+                cout<<"    ";
+            }
+            else if(month[0][j]==3||month[0][j]==11){
+                cout<<"   ";
+            }
+            else if(month[0][j]==6||month[0][j]==7){
+                cout<<" ";
+            }
+            else if(month[0][j]==2||month[0][j]==4||month[0][j]==9||month[0][j]==12||month[0][j]==13){
+                cout<<"  ";
+            }
+                cout<<"    |     ";
+            cout<<month[2][j]<<endl;
+            cout<<"----------------------------------"<<endl;
+        }
+    }
+
       o:cout<<"if you want to search for a specific month's information press \"s\"\nto change the data representation press \"t\"\npress \"c\" to change the data entered \npress \"q\" to leave the program\npress \"a\" to change the the data you want to see\nand do press any thing if you want to continue viewing"<<endl;
     d:cin>>menu2;
 
