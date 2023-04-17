6d9334b3fccdd7bce411154febd8d6b91f917f08
Mikias Goitom <mikiasgoitom7@gmail.com>
2023-04-17 13:05:30 +0300

    Update problem2.cpp

--- project/project1/problem2.cpp
+++ project/project1/problem2.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main() {
     int num=9;
+    
 for(int i = 0; i < (2*num +1); i++){
- for(int j = 0; j < (num*2 + 1); j++)
-   
+ for(int j = 0; j < (num*2 + 1); j++){
+   int num1 = num-abs(num - j);
+   int num2 = num-abs(num - i);
+ }
         cout<<endl;
     }
 return 0;
