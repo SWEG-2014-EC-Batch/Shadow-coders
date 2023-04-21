9a7d6b445aec7a078aa15c3835f0618ebf55f266
Mikias Goitom <mikiasgoitom7@gmail.com>
2023-04-21 21:18:15 +0300

    Update problem1.cpp

--- project/project1/problem1.cpp
+++ project/project1/problem1.cpp
@@ -10,6 +10,7 @@ int main(){
     cout<<"Enter the temp you want to cook with \n";
     cin>>x;
     y=x;
+    
     cout<<"\n Your input tempreature is out of bound";
     
     for(;x>0;x/=10){
@@ -19,27 +20,29 @@ int main(){
     int s[w];
     
     for(int i=0;y>0;y/=10,i++){//used to store our input tempreature in our array
-        s[i]=y%10;}
+        s[i]=y%10;
+    }
+    
     for(int i=0;i<arraySize;i++){
-    if(s[i]==1||s[i]==4||s[i]==7){
-        condition=1;
-        if(i==0){
-        sum1+=(s[i]+1)*pow(10,i);
-        sum2+=(s[i]-1)*pow(10,i);
-        }
-
-  else
-            { 
+        if(s[i]==1||s[i]==4||s[i]==7){
+            
+            condition=1;
+            
+            if(i==0){
+                sum1+=(s[i]+1)*pow(10,i);
+                sum2+=(s[i]-1)*pow(10,i);
+            }
+            else{ 
                 sum1=(s[i]+1)*pow(10,i);
                 sum2=(s[i])*pow(10,i)-1;
             }
-    }
-    else
-        {
-        sum1+=s[i]*pow(10,i);
-        sum2+=s[i]*pow(10,i);
+        }
+        else{
+            sum1+=s[i]*pow(10,i);
+            sum2+=s[i]*pow(10,i);
         }
     }
+    
 if(m!=0){//if our input temperature has the digits 1, 4, or 7
     system("cls");
     cout<<"the least upper bound temperature you can cook with is "<<sum1<<"*c"<<endl<<"the highest lower bound temperature you can cook with is "<<sum2<<"*c"<<endl;
