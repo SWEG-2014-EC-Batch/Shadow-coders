644a8d60279a9b301d5afb0f78fba74cb9cd51ef
MihretDe <124504706+MihretDe@users.noreply.github.com>
2023-04-21 23:04:35 +0300

    Update problem2.cpp

--- project/project1/problem2.cpp
+++ project/project1/problem2.cpp
@@ -5,7 +5,7 @@ int main() {
     int num=9;
     
 for(int i = 0; i < (2*num +1); i++){
- for(int j = 0; j < (num*2 + 1); j++){
+ for(int j = 0; j < (num*2 + 1); j++){//calculates the number of columns
    int num1 = num-abs(num - j);
    int num2 = num-abs(num - i);
  
