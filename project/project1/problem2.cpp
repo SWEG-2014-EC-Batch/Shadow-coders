8fedb9e5570906106368650f89308dc1aacf9424
Mikias Goitom <mikiasgoitom7@gmail.com>
2023-04-22 17:11:35 +0300

    Update problem2.cpp

--- project/project1/problem2.cpp
+++ project/project1/problem2.cpp
@@ -2,25 +2,27 @@
 #include<math.h>
 using namespace std;
 int main() {
-cout<<"Enter a number: ";
-    int num=9;
+    int num;
+    cout<<"Enter a number: ";
     
-for(int i = 0; i < (2*num +1); i++)//calculates the number of rows
-{
- for(int j = 0; j < (num*2 + 1); j++){//calculates the number of columns
-   int num1 = num-abs(num - j);
-   int num2 = num-abs(num - i);
- 
-     if (num1 + num2 <= num){
-                cout<<num1<<" ";
-            }
-    else {//to create the daimond hallow with in the square of numbers
-        if((num1)<10){
-                cout<<"  ";
+    for(int i = 0; i < (2*num +1); i++){//calculates the number of rows
+     for(int j = 0; j < (num*2 + 1); j++){//calculates the number of columns
+       int num1 = num-abs(num - j);
+       int num2 = num-abs(num - i);
+
+        if (num1 + num2 <= num){
+                    cout<<num1<<" ";
                 }
-    }
- }
-        cout<<endl;
+        else {//to create the daimond hallow with in the square of numbers
+            if((num1)<10){
+                    cout<<"  ";
+                    }
+            else if((num1)>9 && (num1)<100){
+                        cout<<"   ";
+                    }
+            }
+        }
+            cout<<endl;
     }
 return 0;
 }
