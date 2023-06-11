d4000bd68c569db40951bfa4ed89cead0c89fc27
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2023-06-11 04:39:59 -0700

    Update question 7.cpp
    removing the infinite looping nature of the program

--- FOP2/homeWork!/question 7.cpp
+++ FOP2/homeWork!/question 7.cpp
@@ -3,12 +3,17 @@ using namespace std;
 double multiplier(double num1, double num2);
 int main()
 {
+    int limit;
     double num1, num2 = 1;
-    while (1 == 1)
+    cout << "How many numbers are going to multiply? \n user: ";
+    cin >> limit;
+    for(int i=1; i<limit; i++)
     {
         cin >> num1;
         num2 = multiplier(num1, num2);
-        cout << "product: " << num2 << endl;
+        if(i==limit){
+            cout << "product: " << num2 << endl;
+        }
     }
 
     return 0;
